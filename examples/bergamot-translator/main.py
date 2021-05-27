c7b08ec6985ba1263de19e399590ae13279e4261
Jerin Philip <jphilip@ed.ac.uk>
2021-05-27 19:57:15 +0100

    Updating things once again

--- examples/bergamot-translator/main.py
+++ examples/bergamot-translator/main.py
@@ -7,10 +7,39 @@ sys.path.insert(0, root)
 
 from ghyamlgen import *
 
-if __name__ == '__main__':
-  on = On(push={"branches": ['main']}, pull_request={"branches": ['main']})
-  env = {'this_repository': 'browsermt/bergamot-translator'}
 
+def RunIfFailed(job):
+  return GitHubExpr(
+      "needs.{jobid}.result == 'failure'".format(jobid=job.fields["name"]))
+
+
+def Always(job):
+  return GitHubExpr("always()")
+
+
+def merge(*xs):
+  ys = []
+  for x in xs:
+    ys.extend(x)
+  return ys
+
+
+def ccache(build):
+  return [
+      CcacheVars(check=GitHubExpr('env.cache_cmd')),
+      GHCache(),
+      CcacheEnv(check=GitHubExpr('env.cache_cmd'),
+                base_dir=GitHubExpr('github.workspace'),
+                directory=GitHubExpr('env.ccache_dir'),
+                compress="true",
+                maxsize="100M"),
+      CCacheProlog(),
+      *build,
+      CCacheEpilog(),
+  ]
+
+
+def ubuntu():
   setup = [
       Checkout(),
       ImportedSnippet(
@@ -30,20 +59,83 @@ if __name__ == '__main__':
                       working_directory="build"),
   ]
 
-  def ccache(build):
-    return [
-        CcacheVars(check=GitHubExpr('env.cache_cmd')),
-        GHCache(),
-        CcacheEnv(check=GitHubExpr('env.cache_cmd'),
-                  base_dir=GitHubExpr('github.workspace'),
-                  directory=os.path.join(GitHubExpr('github.workspace'),
-                                         '.ccache'),
-                  compress="true",
-                  maxsize="100M"),
-        CCacheProlog(),
-        *build,
-        CCacheEpilog(),
-    ]
+  build_epilog = [
+      ImportedSnippet(
+          "Print Versions",
+          "examples/bergamot-translator/native-ubuntu/21-print-versions.sh",
+          working_directory='build'),
+      ImportedSnippet(
+          "Run unit tests",
+          "examples/bergamot-translator/native-ubuntu/30-unit-tests.sh",
+          working_directory='build'),
+  ]
+
+  def build_env(with_cache=False):
+    maybeCcache = '-DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache' if with_cache else ''
+    env = {
+        "gcc":
+            8,
+        "ccache_dir":
+            QuotedExpr(os.path.join(GitHubExpr('github.workspace'), '.ccache')),
+        "cache_cmd":
+            QuotedExpr(
+                'bash ${GITHUB_WORKSPACE}/scripts/ci/compiler-hash.sh %compiler%'
+            ),
+        "cmake":
+            QuotedExpr(
+                '-DCMAKE_BUILD_TYPE=Release -DCOMPILE_TESTS=on {maybeCcache}'.
+                format(maybeCcache=maybeCcache)),
+        # "brt_tags": QuotedExpr("'#native'"),
+        "brt_tags":
+            None,
+    }
+
+    return env
+
+  cached = Job(name='ubuntu',
+               env=build_env(with_cache=True),
+               runs_on='ubuntu-18.04',
+               steps=merge(setup, ccache(build), build_epilog, BRT()))
+
+  fresh = Job(name='ubuntu_fresh',
+              env=build_env(with_cache=False),
+              runs_on='ubuntu-18.04',
+              steps=merge(setup, build, build_epilog, BRT()))
+
+  fresh.needs(job=cached, OpExpr=RunIfFailed),
+
+  log = Job(
+      name='log_needs_context',
+      env=None,
+      runs_on='ubuntu-18.04',
+      steps=[LogContext('needs'),
+             Evaluate("needs.cached.result == 'failure'")])
+
+  log.needs(job=cached, OpExpr=Always)
+  jobs = [cached, fresh, log]
+  jobdict = {job.name(): job for job in jobs}
+  return jobdict
+
+
+def mac():
+  setup = [
+      Checkout(),
+      ImportedSnippet(
+          "Install Dependencies",
+          "examples/bergamot-translator/native-ubuntu/00-install-deps.sh"),
+      ImportedSnippet(
+          "Install MKL",
+          "examples/bergamot-translator/native-ubuntu/01-install-mkl.sh"),
+  ]
+
+  build = [
+      ImportedSnippet(
+          "cmake",
+          "examples/bergamot-translator/native-ubuntu/10-cmake-run.sh"),
+      ImportedSnippet("Build from source",
+                      "examples/bergamot-translator/native-ubuntu/20-build.sh",
+                      working_directory="build"),
+  ]
 
   build_epilog = [
       ImportedSnippet(
@@ -56,44 +148,34 @@ if __name__ == '__main__':
           working_directory='build'),
   ]
 
-  def merge(*xs):
-    ys = []
-    for x in xs:
-      ys.extend(x)
-    return ys
-
   def build_env(with_cache=False):
-      maybeCcache = '-DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache' if with_cache else ''
-      env = {
-          "gcc":
-              8,
-          "CCACHE_DIR":
-              QuotedExpr(os.path.join(GitHubExpr('github.workspace'), '.ccache')),
-          "cache_cmd":
-              QuotedExpr(
-                  'bash ${GITHUB_WORKSPACE}/scripts/ci/compiler-hash.sh %compiler%'
-              ),
-          "cmake":
-              QuotedExpr(
-                  '-DCMAKE_BUILD_TYPE=Release -DCOMPILE_TESTS=on {maybeCcache}'.format(maybeCcache=maybeCcache)
-              ),
-          # "brt_tags": QuotedExpr("'#native'"),
-          "brt_tags":
-              None,
-      }
-      return env
+    maybeCcache = '-DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache' if with_cache else ''
+    env = {
+        "gcc":
+            8,
+        "ccache_dir":
+            QuotedExpr(os.path.join(GitHubExpr('github.workspace'), '.ccache')),
+        "cache_cmd":
+            QuotedExpr(
+                'bash ${GITHUB_WORKSPACE}/scripts/ci/compiler-hash.sh %compiler%'
+            ),
+        "cmake":
+            QuotedExpr(
+                '-DCMAKE_BUILD_TYPE=Release -DCOMPILE_TESTS=on {maybeCcache}'.
+                format(maybeCcache=maybeCcache)),
+        "brt_tags":
+            QuotedExpr("'#mac'"),
+    }
 
-  cached = Job(name='cached',
-               env=build_env(with_cache=True),
-               runs_on='ubuntu-18.04',
-               steps=merge(setup, ccache(build), build_epilog, BRT()))
+    return env
 
-  fresh = Job(name='fresh',
-              env=build_env(with_cache=False),
-              needs=Needs(job=cached, result='failure'),
-              runs_on='ubuntu-18.04',
-              steps=merge(setup, build, build_epilog, BRT()))
 
-  jobs = {"cached": cached, "fresh": fresh}
+if __name__ == '__main__':
+  on = On(push={"branches": ['main']}, pull_request={"branches": ['main']})
+  env = {'this_repository': 'browsermt/bergamot-translator'}
+
+  jobs = {}
+  jobs.update(ubuntu())
+
   workflow = Workflow(name='default', on=on, env=None, jobs=jobs)
   print(yaml.dump(resolve(workflow), sort_keys=False, width=1024))
--- ghyamlgen/gh.py
+++ ghyamlgen/gh.py
@@ -17,41 +17,39 @@ class Workflow(YAMLRenderable):
     self.fields = {"name": name, "on": on, "env": env, "jobs": jobs}
 
 
-class Needs(YAMLRenderable):
-
-  def __init__(self, job, result=None):
-    suffix = '' if result is None else '== \'{}\''.format(result)
-    condition = "needs.{jobname}.result {suffix}".format(
-        jobname=job.fields["name"], suffix=suffix)
-    self.fields = {
-        "if": GitHubExpr(condition),
-        "needs": job.fields["name"],
-    }
-
-
 class Job(YAMLRenderable):
 
-  def __init__(self,
-               name,
-               runs_on,
-               env=None,
-               outputs=None,
-               condition=None,
-               steps=None,
-               needs=None):
-
-    condition = None if needs is None else needs.fields["if"]
-    needed_job = None if needs is None else needs.fields["needs"]
+  def __init__(
+      self,
+      name,
+      runs_on,
+      env=None,
+      outputs=None,
+      condition=None,
+      steps=None,
+  ):
 
     self.fields = {
         "name": name,
         "env": env,
         "runs-on": runs_on,
         "if": condition,
-        "needs": needed_job,
+        "needs": None,
         "steps": steps,
         "outputs": outputs,
     }
+
+  def name(self):
+    return self.fields["name"]
+
+  def needs(self, job, OpExpr):
+    condition = "{}".format(OpExpr(job))
+    fields = {
+        "if": condition,
+        "needs": job.fields["name"],
+    }
+
+    self.fields.update(fields)
 
 
 class Group(YAMLRenderable):
@@ -87,20 +85,47 @@ class JobShellStep(YAMLRenderable):
 class GHCache(YAMLRenderable):
 
   def __init__(self):
+
+    keys = [
+        GitHubExpr('github.job'),
+        GitHubExpr('steps.ccache_vars.outputs.hash'),
+        GitHubExpr('github.ref'),
+        GitHubExpr('steps.ccache_vars.outputs.timestamp')
+    ]
+
+    transform = lambda keys: '-'.join(['ccache'] + keys)
+
     self.fields = {
         "name": "Cache-op for build-cache through ccache",
         "uses": "actions/cache@v2",
         "with": {
             "path":
-                '${{ env.CCACHE_DIR }}',
+                GitHubExpr('env.ccache_dir'),
             "key":
-                "ccache-${{ github.job }}-${{ steps.ccache_vars.outputs.hash }}-${{ github.ref }}-${{ steps.ccache_vars.outputs.timestamp }}",
+                transform(keys),
             "restore-keys":
+                Snippet('\n'.join(
+                    [transform(keys[:-i]) for i in range(len(keys))]))
+        }
+    }
+
+
+class UploadArtifacts(YAMLRenderable):
+
+  def __init__(self):
+    self.fields = {
+        "name": "Upload regression-tests artifacts",
+        "uses": "actions/upload-artifact@v2",
+        "if": GitHubExpr("always()"),
+        "with": {
+            "name":
+                "brt-{}".format(GitHubExpr("github.job")),
+            "path":
                 Snippet('\n'.join([
-                    "ccache-${{ github.job }}-${{ steps.ccache_vars.outputs.hash }}-${{ github.ref }}-",
-                    "ccache-${{ github.job }}-${{ steps.ccache_vars.outputs.hash }}-",
-                    "ccache-${{ github.job }}-",
-                ]))
+                    "bergamot-translator-tests/**/*.expected",
+                    "bergamot-translator-tests/**/*.log",
+                    "bergamot-translator-tests/**/*.out",
+                ])),
         }
     }
 
@@ -114,7 +139,8 @@ class BRT(list):
                      run="make install"),
         JobShellStep(name="Run regression-tests (BRT)",
                      working_directory=working_directory,
-                     run="MARIAN=../build ./run_brt.sh ${{ env.brt_tags }}")
+                     run="MARIAN=../build ./run_brt.sh ${{ env.brt_tags }}"),
+        UploadArtifacts()
     ])
 
 
@@ -127,3 +153,36 @@ class ImportedSnippet(JobShellStep):
     super().__init__(name=name,
                      run=contents,
                      working_directory=working_directory)
+
+
+class HardFailBash(JobShellStep):
+
+  def __init__(self):
+    super().__init__(name="Hard fail to check trigger for other workflow",
+                     run='exit 1',
+                     shell='bash')
+
+
+class LogContext(YAMLRenderable):
+
+  def __init__(self, context):
+    self.fields = {
+        "name": "Dump {} context".format(context),
+        "env": {
+            "{}_CONTEXT".format(context.upper()):
+                GitHubExpr('toJSON({})'.format(context))
+        },
+        "run": "echo ${}_CONTEXT".format(context.upper())
+    }
+
+
+class Evaluate(YAMLRenderable):
+
+  def __init__(self, expr):
+    self.fields = {
+        "name": "Evaluate {}".format(expr),
+        "env": {
+            "EXPR_VALUE": GitHubExpr(expr)
+        },
+        "run": "echo $EXPR_VALUE"
+    }
